8dbd9115b548868f1d9e42eb743445209bb3a7a6
jose <josegonzalezruiz91@gmail.com>
2023-03-14 12:42:20 +0100

    clase telf

---
+++ testExamen/src/main/java/es/corenetworks/dam/testExamen/Telefono.java
@@ -0,0 +1,14 @@
+package es.corenetworks.dam.testExamen;
+
+import es.corenetworks.dam.testExamen.utilidades.NumeroNoValidoException;
+
+public class Telefono {
+	
+	public static void main (String args []) throws NumeroNoValidoException {
+		
+		Agenda agenda1 = new Agenda();
+		
+		agenda1.añadirNumero(64578445);
+	}
+	
+}
