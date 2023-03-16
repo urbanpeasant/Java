208611a8d79eccc08afab71c9381b3b971d63b18
jose <josegonzalezruiz91@gmail.com>
2023-03-16 12:36:44 +0100

    repasoPreExamen

--- ejercicioGestionExcepciones/src/main/java/ejercicioGestionExcepciones/LeerPorTeclado.java
+++ ejercicioGestionExcepciones/src/main/java/ejercicioGestionExcepciones/LeerPorTeclado.java
@@ -1,42 +1,84 @@
+/**
+
+Clase LeerPorTeclado permite la lectura de un carácter por teclado y procesa excepciones
+según el tipo de caracter leido: vocal, número, espacio en blanco o caracter de salida.
+*/
 package ejercicioGestionExcepciones;
 
 import java.util.Scanner;
 
 public class LeerPorTeclado {
-    private char caracterLeido;
-    
-    public char getChar() throws ExcepcionVocal, ExcepcionNumero, ExcepcionBlanco, ExcepcionSalida {
-        Scanner sc = new Scanner(System.in);
-        this.caracterLeido = sc.next().charAt(0);
-        
-        // Procesamiento de excepción según el caracter leido:
-        
-        if (esVocal(this.caracterLeido)) {
-            throw new ExcepcionVocal("Se ha leido una vocal");
-        } else if (esNumero(this.caracterLeido)) {
-            throw new ExcepcionNumero("Se ha leido un número");
-        } else if (esBlanco(this.caracterLeido)) {
-            throw new ExcepcionBlanco("Se ha leido un espacio en blanco");
-        } else if (esSalida(this.caracterLeido)) {
-            throw new ExcepcionSalida("Se ha leido el caracter de salida");
-        }
-        
-        return this.caracterLeido;
-    }
-    private boolean esVocal(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
-    }
-    
-    private boolean esNumero(char c) {
-        return Character.isDigit(c);
-    }
-    
-    private boolean esBlanco(char c) {
-        return Character.isWhitespace(c);
-    }
-    
-    private boolean esSalida(char c) {
-        return c == 'x';
-    }
-    
+
+	private char caracterLeido;
+
+	/**
+	 * Lee un carácter desde la entrada estándar del sistema y verifica si es una
+	 * vocal, un número, un espacio en blanco o un caracter de salida, lanzando una
+	 * excepción correspondiente en cada caso.
+	 * 
+	 * @return el caracter leido
+	 * @throws ExcepcionVocal  si se ha leido una vocal
+	 * @throws ExcepcionNumero si se ha leido un número
+	 * @throws ExcepcionBlanco si se ha leido un espacio en blanco
+	 * @throws ExcepcionSalida si se ha leido el caracter de salida
+	 */
+	public char getChar() throws ExcepcionVocal, ExcepcionNumero, ExcepcionBlanco, ExcepcionSalida {
+		Scanner sc = new Scanner(System.in);
+		this.caracterLeido = sc.next().charAt(0);
+
+		// Procesamiento de excepción según el caracter leido:
+
+		if (esVocal(this.caracterLeido)) {
+			throw new ExcepcionVocal("Se ha leido una vocal");
+		} else if (esNumero(this.caracterLeido)) {
+			throw new ExcepcionNumero("Se ha leido un número");
+		} else if (esBlanco(this.caracterLeido)) {
+			throw new ExcepcionBlanco("Se ha leido un espacio en blanco");
+		} else if (esSalida(this.caracterLeido)) {
+			throw new ExcepcionSalida("Se ha leido el caracter de salida");
+		}
+
+		return this.caracterLeido;
+	}
+
+	/**
+	 * Verifica si el caracter dado es una vocal (mayúscula o minúscula).
+	 * 
+	 * @param c el caracter a evaluar
+	 * @return true si es una vocal, false en caso contrario
+	 */
+	private boolean esVocal(char c) {
+		return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O'
+				|| c == 'U';
+	}
+
+	/**
+	 * Verifica si el caracter dado es un número.
+	 * 
+	 * @param c el caracter a evaluar
+	 * @return true si es un número, false en caso contrario
+	 */
+	private boolean esNumero(char c) {
+		return Character.isDigit(c);
+	}
+
+	/**
+	 * Verifica si el caracter dado es un espacio en blanco.
+	 * 
+	 * @param c el caracter a evaluar
+	 * @return true si es un espacio en blanco, false en caso contrario
+	 */
+	private boolean esBlanco(char c) {
+		return Character.isWhitespace(c);
+	}
+
+	/**
+	 * Verifica si el caracter dado es el caracter de salida "x".
+	 * 
+	 * @param c el caracter a evaluar
+	 * @return true si es el caracter de salida, false en caso contrario
+	 */
+	private boolean esSalida(char c) {
+		return c == 'x';
+	}
 }
--- testExamen/src/main/java/es/corenetworks/dam/testExamen/Agenda.java
+++ testExamen/src/main/java/es/corenetworks/dam/testExamen/Agenda.java
@@ -8,102 +8,103 @@ import es.corenetworks.dam.testExamen.utilidades.PosicionVaciaException;
 import es.corenetworks.dam.testExamen.utilidades.Utilidades;
 
 public class Agenda {
-	//Atributos de la clase.
-
-	private int[] agenda;
-	public int elementos = 0;
-
-	// Constructor vacío
+	
+	//atributos de la clase.
+	private int [] agenda; //array de enteros
+	public int elementos; // contador para los elementos de la agenda
+	
+	
+	//Constructor sin parámetros que da como tamaño al array el dado por la constante previamente creada.
 	public Agenda() {
-		agenda = new int[Constantes.TAMAÑO_AGENDA];
-
+		
+		agenda = new int [Constantes.TAMAÑO_AGENDA];
 	}
-
-	// Constructor con parámetro tamaño agenda
-	public Agenda(int tamano) {
-		agenda = new int[tamano];
+	
+	//Constructor con el parámetro tamaño agenda
+	
+	public Agenda (int tamanyo) {
+		this.agenda = new int [tamanyo];
 	}
 	
 	//Método para obtener el tamaño de la agenda
+	
 	public int getTamanyoAgenda() {
 		return elementos;
 	}
 	
-	//Método para comprobar el número de teléfono. Si no es válido, lanza una excepcón. 
+	//Método para comprobar el teléfono. Si no es válido lanza una excepción.
 	
-	private void compruebaTelf(int numTelf) throws NumeroNoValidoException {
-		if (Utilidades.getLongitudNumero(numTelf) != 8)
+	private void compruebaTelf(int numTelf) throws NumeroNoValidoException{
+		if(Utilidades.getLongitudNumero(numTelf)!=8)
 			throw new NumeroNoValidoException(Constantes.NUMERO_NO_VALIDO_EXCEPTION);
 	}
 	
-	//Método para añadir número de teléfono. Si no es válido, lanza una excepción.
-	public void añadirNumero(int numTelf) throws NumeroNoValidoException {
+	//Método para añadir el telf. Usa el método compruebaTelf para ver si es válido. Si es válido, añade el num. Si no, lanza excepción.
+	public void añadirNumero(int numTelf) throws NumeroNoValidoException{
 		compruebaTelf(numTelf);
 		boolean agregado = false;
-		for (int i = 0; i < agenda.length; i++) {
+		for (int i =0; i<agenda.length; i++) {
 			if (agenda[i] == 0) {
-				agenda[i] = numTelf;
+				agenda [i]=numTelf;
 				agregado = true;
-				elementos++;
+				elementos ++;
 				break;
 			}
 		}
+		
 		if (agregado == false) {
-			System.out.println("La agenda está llena.");
+			System.out.println("Agenda llena");
 		}
 	}
 	
-	//Método para buscar un número de teléfono. Si no encuentra el número, lanza una excepción.
-	public int buscarNumero(int numTelf) throws NumeroNotFoundException {
-		for (int i = 0; i < agenda.length; i++) {
-			if (agenda[i] == numTelf) {
+	//Método para buscar num en la agenda. Si no encuentra num lanza excepcion.
+	
+	public int buscarNumero (int numTelf) throws NumeroNotFoundException {
+		for (int i = 0; i<agenda.length;i++) {
+			if (agenda[i]==numTelf) {
 				return i;
 			}
 		}
 		throw new NumeroNotFoundException(Constantes.NUMERO_NOT_FOUND_EXCEPTION);
 	}
 	
+	//Método para eliminar num de la agenda. Recibe un telf y lo elimina, poniendo un 0 en su posicion. Si el num no es válido lanza la expceion pertinente.Devuelve true si ha borrado y false si no.
 	
-	//Método para eliminar un número de teléfono. Si el teléfono no es válido, lanza una excepción.
-	public void eliminarNumero(int numTelf) throws NumeroNoValidoException {
+	public boolean eliminarNumero(int numTelf) throws NumeroNoValidoException{
 		compruebaTelf(numTelf);
-		boolean borrado = false;
-		for (int i = 0; i < agenda.length; i++) {
-			if (agenda[i] == numTelf) {
-				agenda[i] = 0;
-				elementos--;
-				borrado = true;
-				break;
-			}
-		}
-		if (borrado == false) {
-			System.out.println("El número de teléfono no existe.");
+		int posicion;
+		try {
+			posicion = buscarNumero(numTelf);
+			agenda[posicion]=0;
+			System.out.println("numero "+ numTelf + "borrado");
+			elementos --;
+			return true;
+		} catch (NumeroNotFoundException e) {
+			return false;
 		}
 	}
 	
-	//Método que recibe un entero que representa la posición en el array. Si la posición no es válida o está vacía, lanza la excepción correspondiente.
-	public int getNumeroEnPosicion(int posicion) throws PosicionNoValidaException, PosicionVaciaException{
-		if (posicion > agenda.length - 1)
+	//Método que recibe un int que representa la posicion en el array. Si no es válida o está vacia, lanza excepción.
+	
+	public int getNumeroEnPosicion(int posicion) throws PosicionNoValidaException, PosicionVaciaException {
+		if (posicion > agenda.length -1)
 			throw new PosicionNoValidaException(Constantes.POSICION_NO_VALIDA_EXCEPTION);
 		if (agenda[posicion] == 0) {
 			throw new PosicionVaciaException(Constantes.POSICION_VACIA_EXCEPTION);
 		}
-
+		
 		return agenda[posicion];
 	}
 
-	// Asignando el valor a cero devolverá true si es cero y false en caso
-	// contrario.
-	//Este método devuelve true si la agenda está vacía y false si tiene números.
 	public boolean estaVacia() {
-		
-		for(int i =0; i<agenda.length; i++) {
+		for (int i=0; i<agenda.length; i++) {
 			if (agenda[i]!=0) {
 				return false;
 			}
-		} return true;
+			
+			
+			}return true;
+		}
 	}
 
-	
-	
-}
+
